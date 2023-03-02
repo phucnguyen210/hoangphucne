96a581379290abf18e35bbc3a1406b9726e49246
hoangphuc03 <94794932+hoangphuc03@users.noreply.github.com>
2023-03-02 22:46:34 +0700

    Add files via upload

---
+++ Bai7.java
@@ -0,0 +1,11 @@
+package Chung;
+import java.util.Scanner;
+public class Bai7 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		 
+		 for(int i=1;i<=20;i++)
+			 System.out.println(i);
+	}
+}
